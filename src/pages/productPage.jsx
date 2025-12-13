f5db5be88a251abaa6dc2af38cbf2c9834d5acb3
Gaurawa Wickramasinghe <gaurawamihiranga@gmail.com>
2025-12-13 13:43:32 +0530

    day 11🔧

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -3,42 +3,67 @@ import { useEffect, useState } from "react";
 import Loader from "../components/loader";
 import ProductCard from "../components/productCard";
 
-export default function ProductPage(){
+export default function ProductPage() {
+	const [products, setProducts] = useState([]);
+	const [loaded, setLoaded] = useState(false);
 
-    const [products, setProducts] = useState([]);
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-
-        if(!loaded){
-            axios
+	useEffect(() => {
+		if (!loaded) {
+			axios
 				.get(import.meta.env.VITE_BACKEND_URL + "/products")
 				.then((response) => {
 					console.log(response.data);
 					setProducts(response.data);
 					setLoaded(true);
 				});
-        }
+		}
+	}, []);
 
-    },[])
+	return (
+		<div className="w-full h-[calc(100vh-100px)]">
+			{!loaded ? (
+				<Loader />
+			) : (
+				<div className="w-full flex justify-center p-4 flex-row flex-wrap  ">
+					<div className="w-full h-[100px] sticky top-0 bg-white flex justify-center items-center mb-4 shadow-md z-10">
+						<input
+							type="text"
+							placeholder="Search products..."
+							className="w-1/2 px-4 py-2 border border-secondary/30 rounded-lg outline-none"							
+							onChange={async (e) => {
 
-    return(
-        <div className="w-full h-[calc(100vh-100px)]">
-            {
-                !loaded?<Loader/>:
-                <div className="w-full flex justify-center p-4 flex-row flex-wrap">
-                    {
-                        products.map(
-                            (item)=>{
-                                return(
-                                    <ProductCard key={item.productID} product={item} />
-                                )
-                            }
-                        )
-                    }
-                </div>
-            }
+								if (e.target.value == "") {
+                                    setLoaded(false);
+									await axios
+										.get(import.meta.env.VITE_BACKEND_URL + "/products")
+										.then((response) => {
+											console.log(response.data);
+											setProducts(response.data);
+											setLoaded(true);
+										});
+                                    setLoaded(true);
+								}else{
+                                    await axios
+                                        .get(
+                                            import.meta.env.VITE_BACKEND_URL +
+                                                "/products/search/" +
+                                                e.target.value
+                                        )
+                                        .then((response) => {
+                                            console.log(response.data);
+                                            setProducts(response.data);
+                                        });
+                                    setLoaded(true);
+                                }
+							}}
+						/>
+					</div>
 
-        </div>
-    )
+					{products.map((item) => {
+						return <ProductCard key={item.productID} product={item} />;
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
